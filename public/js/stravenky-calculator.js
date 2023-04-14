0431e2eb38e1b9ddfb7717c3bb71f7f48d4b18d2
Adam Sucharda <95930302+asuchardaa@users.noreply.github.com>
2023-04-14 20:47:10 +0200

    uz vypisuje excel po listech

--- public/js/stravenky-calculator.js
+++ public/js/stravenky-calculator.js
@@ -1,19 +1,20 @@
 // Wait for the DOM to load
 document.addEventListener('DOMContentLoaded', function() {
-    // Get the upload form and result table
+// Get the upload form and result table
     var form = document.getElementById('upload-form');
     var resultTable = document.getElementById('result-table');
     var uploadBtn = document.querySelector('#upload-form button[type="submit"]');
 
 
     document.getElementById("excel-file").addEventListener('change', function() {
-       if (this.files[0]) {
-           uploadBtn.disabled = false;
-       } else {
-           uploadBtn.disabled = true;
-       }
+        if (this.files[0]) {
+            uploadBtn.disabled = false;
+        } else {
+            uploadBtn.disabled = true;
+        }
     });
-    // Handle form submit
+
+// Handle form submit
     form.addEventListener('submit', function(event) {
         // Prevent default form behavior
         event.preventDefault();
@@ -48,75 +49,74 @@ document.addEventListener('DOMContentLoaded', function() {
             resultTable.appendChild(table);
         };
     });
-});
+
 
 // Function to process the workbook data and generate the result table
-function processWorkbook(workbook) {
-    // Create a table element
-    var table = document.createElement('table');
-    table.classList.add('table');
+    function processWorkbook(workbook) {
+        // Create a table element
+        var table = document.createElement('table');
+        table.classList.add('table');
 
-    // Create a table header row
-    var headerRow = table.insertRow();
-    var headers = ['Jméno', 'Příjmení', 'Počet hodin v práci', 'Počet stravenek'];
-    for (var i = 0; i < headers.length; i++) {
-        var headerCell = document.createElement('th');
-        headerCell.innerText = headers[i];
-        headerRow.appendChild(headerCell);
-    }
-
-    // Get the sheet data
-    var sheet = workbook.Sheets[workbook.SheetNames[0]];
-
-    // Convert the sheet data to JSON format
-    var jsonSheetData = XLSX.utils.sheet_to_json(sheet);
-
-    // Create an object to store the total number of hours worked and meal vouchers
-    var employeeData = {};
-
-    // Loop through each row in the sheet data
-    jsonSheetData.forEach(function(row) {
-        // Check if the row corresponds to a working day with meal vouchers
-        if ((row['Typ'] === 'SC' || row['Typ'] === 'P') && row['Příchod'] && row['Odchod']) {
-            // Get the employee's name and surname
-            var name = row['Jméno'];
-            var surname = row['Příjmení'];
-
-            // Calculate the number of hours worked
-            var hoursWorked = row['Odchod'] - row['Příchod'];
-
-            // Add the number of hours worked to the employee's total
-            if (!employeeData[name + ' ' + surname]) {
-                employeeData[name + ' ' + surname] = {
-                    hoursWorked: 0,
-                    mealVouchers: 0
-                };
-            }
-            employeeData[name + ' ' + surname].hoursWorked += hoursWorked;
-
-            // Check if the employee is entitled to a meal voucher
-            if (hoursWorked >= 7) {
-                employeeData[name + ' ' + surname].mealVouchers += 1;
-            }
+        // Create a table header row
+        var headerRow = table.insertRow();
+        var headers = ['Jméno', 'Příjmení', 'Počet hodin v práci', 'Počet stravenek'];
+        for (var i = 0; i < headers.length; i++) {
+            var headerCell = document.createElement('th');
+            headerCell.innerText = headers[i];
+            headerRow.appendChild(headerCell);
         }
-    });
 
-    // Loop through the employee data and create a table row for each employee
-    Object.keys(employeeData).forEach(function(key) {
-        var dataRow = table.insertRow();
-        var nameCell = dataRow.insertCell();
-        var nameParts = key.split(' ');
-        nameCell.innerText = nameParts[0];
-        var surnameCell = dataRow.insertCell();
-        surnameCell.innerText = nameParts[1];
-        var hoursWorkedCell = dataRow.insertCell();
-        hoursWorkedCell.innerText = employeeData[key].hoursWorked;
-        var mealVouchersCell = dataRow.insertCell();
-        mealVouchersCell.innerText = employeeData[key].mealVouchers;
-    });
+        // Create an object to store the total number of hours worked and meal vouchers
+        var employeeData = {};
 
-    // Add the table to the HTML document
-    document.body.appendChild(table);
-}
+        // Loop through each sheet in the workbook
+        workbook.SheetNames.forEach(function(sheetName) {
+            var sheet = workbook.Sheets[sheetName];
+            // Convert the sheet data to JSON format
+            var jsonSheetData = XLSX.utils.sheet_to_json(sheet);
 
+            // Loop through each row in the sheet data
+            jsonSheetData.forEach(function(row) {
+                // Check if the row corresponds to a working day with meal vouchers
+                if ((row['Typ'] === 'SC' || row['Typ'] === 'P') && row['Příchod'] && row['Odchod']) {
+                    // Get the employee's name and surname
+                    var name = row['Jméno'];
+                    var surname = row['Příjmení'];
 
+                    // Calculate the number of hours worked
+                    var hoursWorked = row['Odchod'] - row['Příchod'];
+
+                    // Add the number of hours worked to the employee's total
+                    if (!employeeData[name + ' ' + surname]) {
+                        employeeData[name + ' ' + surname] = {
+                            hoursWorked: 0,
+                            mealVouchers: 0
+                        };
+                    }
+                    employeeData[name + ' ' + surname].hoursWorked += hoursWorked;
+
+                    // Check if the employee is entitled to a meal voucher
+                    if (hoursWorked >= 7) {
+                        employeeData[name + ' ' + surname].mealVouchers += 1;
+                    }
+                }
+            });
+        });
+
+        // Loop through the employee data and create a table row for each employee
+        Object.keys(employeeData).forEach(function(key) {
+            var dataRow = table.insertRow();
+            var nameCell = dataRow.insertCell();
+            var nameParts = key.split(' ');
+            nameCell.innerText = nameParts[0];
+            var surnameCell = dataRow.insertCell();
+            surnameCell.innerText = nameParts[1];
+            var hoursWorkedCell = dataRow.insertCell();
+            hoursWorkedCell.innerText = employeeData[key].hoursWorked;
+            var mealVouchersCell = dataRow.insertCell();
+            mealVouchersCell.innerText = employeeData[key].mealVouchers;
+        });
+
+        return table;
+    }
+});
