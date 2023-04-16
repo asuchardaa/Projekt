5fdc6e9379d8acbc1c6beed8b27038380be07bfb
Adam Sucharda <95930302+asuchardaa@users.noreply.github.com>
2023-04-16 18:24:42 +0200

    uz pocitame cestovni nahrady + error handling

--- public/js/amends-calculator.js
+++ public/js/amends-calculator.js
@@ -1,14 +1,42 @@
 $(document).ready(function () {
-    $('#calculator').submit(function (event) {
+    // cache elements for better performance
+    var $calculator = $('#calculator');
+    var $fuelPriceCheck = $('#fuelPriceCheck');
+    var $fuelTypeWrapper = $('#fuelTypeWrapper');
+    var $fuelPriceWrapper = $('#fuelPriceWrapper');
+    var $errorMessage = $('#error-container');
+
+    // add event listener to fuelPriceCheck select
+    $fuelPriceCheck.on('change', function () {
+        if ($(this).val() === 'yes') {
+            $fuelTypeWrapper.hide();
+            $fuelPriceWrapper.show();
+        } else {
+            $fuelTypeWrapper.show();
+            $fuelPriceWrapper.hide();
+        }
+    });
+
+    // add submit event listener to calculator form
+    $calculator.submit(function (event) {
         event.preventDefault();
         var distance = parseFloat($('#distance').val());
-        var fuelPriceCheck = $('input[name="fuelPriceCheck"]:checked').val();
+        var fuelPriceCheck = $fuelPriceCheck.val();
         var fuelType = $('#fuelType').val();
-        var fuelConsumption = parseFloat($('#fuelConsumption').val());
         var fuelPrice = 0;
         var wearAndTearPrice = 0;
+        var errorMessageText = '';
+
+        if (isNaN(distance) || distance <= 0) {
+            errorMessageText += '<p>Zadejte platný počet najetých kilometrů!</p>';
+        }
 
         if (fuelPriceCheck === 'yes') {
+            fuelPrice = parseFloat($('#fuelPrice').val());
+            if (isNaN(fuelPrice) || fuelPrice <= 0) {
+                errorMessageText += '<p>Zadejte platnou cenu paliva!</p>';
+            }
+        } else {
             switch (fuelType) {
                 case 'natural95':
                     fuelPrice = 31.50;
@@ -22,14 +50,33 @@ $(document).ready(function () {
                 case 'electric':
                     fuelPrice = 6.00;
                     break;
+                default:
+                    errorMessageText += '<p>Zadejte platný druh paliva!</p>';
+                    break;
             }
         }
 
         wearAndTearPrice = 5.20 * distance;
 
-        var fuelCost = fuelPrice * distance * fuelConsumption / 100;
-        var totalCost = fuelCost + wearAndTearPrice;
+        var fuelConsumption = parseFloat($('#fuelConsumption').val());
+        if (isNaN(fuelConsumption) || fuelConsumption <= 0) {
+            errorMessageText += '<p>Zadejte platnou spotřebu paliva!</p>';
+        }
 
-        $('#result').html('Cestovní náklady celkem: ' + totalCost.toFixed(2) + ' Kč. Cestovní náklady: pohonné hmoty ' + fuelCost.toFixed(2) + ' Kč a opotřebení automobilu ' + wearAndTearPrice.toFixed(2) + ' Kč.');
+        if (errorMessageText !== '') {
+            $errorMessage.html('<p class="error">' + errorMessageText.toUpperCase() + '</p>');
+        } else {
+            $errorMessage.empty();
+            var fuelCost = fuelPrice * distance * fuelConsumption / 100;
+            var totalCost = fuelCost + wearAndTearPrice;
+
+            var table = '<table class="table table-bordered">';
+            table += '<tr><td><b>Cestovní náklady celkem:</b></td><td><strong>' + totalCost.toFixed(2) + ' Kč</strong></td></tr>';
+            table += '<tr><td>Cestovní náklady: pohonné hmoty</td><td>' + fuelCost.toFixed(2) + ' Kč</td></tr>';
+            table += '<tr><td>Cestovní náklady: opotřebení automobilu</td><td>' + wearAndTearPrice.toFixed(2) + ' Kč</td></tr>';
+            table += '</table>';
+
+            $('#result').html(table);
+        }
     });
 });
