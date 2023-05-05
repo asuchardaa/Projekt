1fc0a97bb2758593b4d5417cc1a521abb20f9459
Adam Sucharda <95930302+asuchardaa@users.noreply.github.com>
2023-05-05 12:33:25 +0200

    formatted more readable code

--- public/js/stravenky-calculator.js
+++ public/js/stravenky-calculator.js
@@ -1,6 +1,4 @@
-// Wait for the DOM to load
 document.addEventListener('DOMContentLoaded', function() {
-// Get the upload form and result table
     var form = document.getElementById('upload-form');
     var resultTable = document.getElementById('result-table');
     var uploadBtn = document.querySelector('#upload-form button[type="submit"]');
@@ -14,50 +12,30 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-// Handle form submit
     form.addEventListener('submit', function(event) {
-        // Prevent default form behavior
         event.preventDefault();
-
-        // Get the uploaded file
         var file = document.getElementById('excel-file').files[0];
-
-        // If no file was selected, show an error message and return
         if (!file) {
             document.getElementById('file-error').classList.remove('d-none');
             return;
         }
-
-        // Hide any existing error message
         document.getElementById('file-error').classList.add('d-none');
 
-        // Read the file using FileReader
         var reader = new FileReader();
         reader.readAsBinaryString(file);
-
-        // Handle file reading completion
         reader.onload = function(event) {
-            // Parse the file data using XLSX
             var data = event.target.result;
             var workbook = XLSX.read(data, {type: 'binary'});
-
-            // Process the data and generate the result table
             var table = processWorkbook(workbook);
-
-            // Set the result table in the div
             resultTable.innerHTML = '';
             resultTable.appendChild(table);
         };
     });
 
-
-// Function to process the workbook data and generate the result table
     function processWorkbook(workbook) {
-        // Create a table element
         var table = document.createElement('table');
         table.classList.add('table');
 
-        // Create a table header row
         var headerRow = table.insertRow();
         var headers = ['Jméno', 'Příjmení', 'Počet hodin v práci', 'Počet stravenek'];
         for (var i = 0; i < headers.length; i++) {
@@ -66,28 +44,17 @@ document.addEventListener('DOMContentLoaded', function() {
             headerRow.appendChild(headerCell);
         }
 
-        // Create an object to store the total number of hours worked and meal vouchers
         var employeeData = {};
         var celkemStravenek = 0;
 
-        // Loop through each sheet in the workbook
         workbook.SheetNames.forEach(function(sheetName) {
             var sheet = workbook.Sheets[sheetName];
-            // Convert the sheet data to JSON format
             var jsonSheetData = XLSX.utils.sheet_to_json(sheet);
-
-            // Loop through each row in the sheet data
             jsonSheetData.forEach(function(row) {
-                // Check if the row corresponds to a working day with meal vouchers
                 if ((row['Typ'] === 'P') && row['Příchod'] && row['Odchod']) {
-                    // Get the employee's name and surname
                     var name = row['Jméno'];
                     var surname = row['Příjmení'];
-
-                    // Calculate the number of hours worked
                     var hoursWorked = row['Odchod'] - row['Příchod'];
-
-                    // Add the number of hours worked to the employee's total
                     if (!employeeData[name + ' ' + surname]) {
                         employeeData[name + ' ' + surname] = {
                             hoursWorked: 0,
@@ -96,7 +63,6 @@ document.addEventListener('DOMContentLoaded', function() {
                     }
                     employeeData[name + ' ' + surname].hoursWorked += hoursWorked;
 
-                    // Check if the employee is entitled to a meal voucher
                     if (hoursWorked >= 5) {
                         employeeData[name + ' ' + surname].mealVouchers += 1;
                         celkemStravenek += 1;
@@ -105,7 +71,6 @@ document.addEventListener('DOMContentLoaded', function() {
             });
         });
 
-        // Loop through the employee data and create a table row for each employee
         Object.keys(employeeData).forEach(function(key) {
             var dataRow = table.insertRow();
             var nameCell = dataRow.insertCell();
@@ -124,8 +89,6 @@ document.addEventListener('DOMContentLoaded', function() {
         var totalLabelCell = totalRow.insertCell();
         totalLabelCell.classList.add('table-total-label');
         totalLabelCell.innerText = 'Celkem stravenek:';
-        // totalLabelCell.style.fontWeight = 'bold';
-        // totalLabelCell.style.textAlign = 'left';
         var spacerCell1 = totalRow.insertCell();
         var spacerCell2 = totalRow.insertCell();
         totalLabelCell.colSpan = 1;
--- public/js/submit-form.js
+++ public/js/submit-form.js
@@ -1,9 +1,7 @@
 const form = document.getElementById('contact-form');
 
 form.addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the form from submitting
-
-    // Display a success message
+    event.preventDefault();
     const message = document.createElement('p');
     message.textContent = 'Vaše zpráva byla úspěšně odeslána.';
     message.style.color = 'red';
@@ -13,7 +11,5 @@ form.addEventListener('submit', function(event) {
     message.style.marginTop = '1rem';
     message.style.marginBottom = '1rem';
     form.appendChild(message);
-
-    // Clear the form fields
     form.reset();
 });
